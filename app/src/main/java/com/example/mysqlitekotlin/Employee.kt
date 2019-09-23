dfcafa21c791255192a377fd4eecfbbb9b6f57fb
MohammedOss92 <abdallahomar1992@gmail.com>
2019-09-23 19:36:34 +0300

    Sqlite

---
+++ app/src/main/java/com/example/mysqlitekotlin/Employee.kt
@@ -0,0 +1,4 @@
+package com.example.mysqlitekotlin
+
+class Employee(val id:String ,val firstname:String ,val lastname:String ,
+               val age:String,val address:String,val department:String)
